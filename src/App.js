e844c9b1943b1fade73fba34f18496538b443275
nikalomiris <nikalomiris@gmail.com>
2019-07-27 10:15:23 -0400

    Part 5 bloglist frontend step 2 solution

--- src/App.js
+++ src/App.js
@@ -18,12 +18,24 @@ function App() {
         fetchBlogs();
     }, []);
 
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON);
+            setUser(user);
+        }
+    }, []);
+
     const handleLogin = async (event) => {
         event.preventDefault();
         try {
             const user = await loginService.login({
                 username, password
             });
+
+            window.localStorage.setItem(
+                'loggedBlogappUser', JSON.stringify(user)
+            );
 
             setUser(user);
             setUsername('');
@@ -41,6 +53,11 @@ function App() {
         setPassword(event.target.value);
     };
 
+    const handleLogout = () => {
+        setUser('');
+        window.localStorage.clear();
+    }
+
     if (user === '') {
         return (
             <div className="App">
@@ -57,12 +74,13 @@ function App() {
     }
     return (
         <div className="App">
-                <header className="App-header">
-                    <h1>Bloglist App</h1>
-                </header>
-                <h2>Blogs</h2>
-                <h3>{user.name} is logged in</h3>
-                {blogs.map(blog => <Blog key={blog.id} title={blog.title} author={blog.author} />)}
+            <header className="App-header">
+                <h1>Bloglist App</h1>
+            </header>
+            <h2>Blogs</h2>
+            <h3>{user.name} is logged in</h3>
+            <button onClick={handleLogout}>Logout</button>
+            {blogs.map(blog => <Blog key={blog.id} title={blog.title} author={blog.author} />)}
         </div>
     );
 }
